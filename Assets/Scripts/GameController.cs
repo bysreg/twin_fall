e43866e54ac7b598aaab0d6df7cbf8a02b641702
hilman <hilmanbeyri@gmail.com>
2014-10-01 21:03:53 -0400

    - early implementation of the gameplay

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public bool simulateWithMouse;
+
+	private GameObject player;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update()
+	{
+		if (simulateWithMouse)
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Vector3 point = ray.GetPoint(10);
+			player.transform.position = point;
+		}
+	}
+
+}
