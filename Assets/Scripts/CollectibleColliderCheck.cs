a750f1bce6d090db81ec2a74fa80a18573ed71ad
hilman <hilmanbeyri@gmail.com>
2014-10-04 18:01:36 -0400

    - feather / collectible system

---
+++ Assets/Scripts/CollectibleColliderCheck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleColliderCheck : MonoBehaviour {
+
+	public AudioClip hitCollClip;
+	
+	private GameController gameController;
+	
+	private bool isHit;
+
+	void Awake()
+	{
+		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player" && !isHit) 
+		{
+			AudioSource.PlayClipAtPoint (hitCollClip, other.gameObject.transform.position);
+			isHit = true;
+		}
+	}
+
+	public bool IsHit()
+	{
+		return isHit;
+	}
+}
