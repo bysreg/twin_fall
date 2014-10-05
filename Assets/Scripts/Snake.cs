72f7c74379313e98b8a3924857c68065ea28388d
bysreg <hilman09121991@gmail.com>
2014-10-05 13:41:47 -0400

    - snake animation

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : MonoBehaviour {
+
+	public float frameTime;
+	public Texture2D[] frames;
+
+	private float time;
+	private int frameIndex;
+	private MeshRenderer meshRenderer;
+
+	void Awake()
+	{
+		meshRenderer = GetComponentInChildren<MeshRenderer>();
+	}
+
+	void Update()
+	{
+		time += Time.deltaTime;
+		if(time >= frameTime)
+		{
+			time -= frameTime;
+
+			//change frame
+			frameIndex = (frameIndex + 1) % frames.Length;
+			meshRenderer.material.mainTexture = frames[frameIndex];
+		}
+	}
+}
