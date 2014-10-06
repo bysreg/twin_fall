feb46b9f63300d02c898237ffc317d54891d5236
Vivek <vivek.v.sagar@gmail.com>
2014-10-06 14:11:22 -0400

    Added intro scene and got combo numbers to show

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroController : MonoBehaviour {
+
+	public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+		movie = renderer.material.mainTexture as MovieTexture;
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > movie.duration)
+			Application.LoadLevel(1);
+	}
+}
