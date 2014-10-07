2c27efce218dea732b62cc66d2a3f7648db1c286
Vivek <vivek.v.sagar@gmail.com>
2014-10-06 20:26:32 -0400

    Retry works.. but weird time!

---
+++ Assets/Scripts/RetryButtonController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetryButtonController : MonoBehaviour {
+
+	void OnGUI() {
+		GUI.backgroundColor = Color.clear;
+		if (GUI.Button(new Rect(0, 0, 5000, 5000), ""))
+			Application.LoadLevel(1);
+		
+	}
+}
